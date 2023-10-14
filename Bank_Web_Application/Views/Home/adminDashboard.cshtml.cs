d0b8926141c840b230d913dd22444969426444e3
Vinuk <vinukman@gmail.com>
2023-10-14 12:32:24 +0530

    Login Page Complete

---
+++ Bank_Web_Application/Views/Home/adminDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank_Web_Application.Views.Home
+{
+    public class adminDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
