571dd499ffcdf7b5dcac32d990b8739fae268375
Vinuk <vinukman@gmail.com>
2023-10-14 11:09:01 +0530

    Webpage

---
+++ Bank_Web_Application/Views/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank_Web_Application.Views
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
