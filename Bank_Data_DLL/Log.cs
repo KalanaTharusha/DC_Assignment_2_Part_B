1ef5cd4199908f1b94669143750e8997dce245fb
Kalana Tharusha <kalanatharusha@yahoo.com>
2023-10-15 04:12:29 +0530

    Update function bug fix

---
+++ Bank_Data_DLL/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Data_DLL
+{
+    public class Log
+    {
+        public int LogId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Action { get; set; }
+        public string LogMessage { get; set; }
+    }
+}
