a39cfab2392f403dc38a8d1db6fd9f5e01eb956a
Vinuk <vinukman@gmail.com>
2023-10-14 19:19:18 +0530

    Admin, User and Login Pages

--- Bank_Web_Application/Controllers/HomeController.cs
+++ Bank_Web_Application/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Bank_Web_Application.Controllers
         {
             return View();
         }
+        public IActionResult Admin()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
