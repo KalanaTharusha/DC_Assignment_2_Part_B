238606b4a91c9318c1e3d0e55bb87953df3ab63d
Kalana Tharusha <kalanatharusha@yahoo.com>
2023-10-14 10:25:43 +0530

    business tier

--- Bank_Web_Application/Controllers/BankApiController.cs
+++ Bank_Web_Application/Controllers/BankApiController.cs
@@ -18,7 +18,19 @@ namespace Bank_Web_Application.Controllers
             RestRequest request = new RestRequest("api/users", Method.Post);
             request.AddBody(reqBody);
             RestResponse response = client.Execute(request);
-            return Ok();
+            User user = JsonConvert.DeserializeObject<User>(response.Content);
+
+            request = new RestRequest("api/accounts", Method.Post);
+            Account account = new Account();
+            account.AccountNo = (int)((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+            account.Balance = 0;
+            account.UserId = user.UserId;
+            request.AddBody(account);
+            response = client.Execute(request);
+
+            Account createdAccount = JsonConvert.DeserializeObject<Account>(response.Content);
+
+            return new ObjectResult(createdAccount) { StatusCode = 201};
         }
 
         [HttpPost]
@@ -39,6 +51,55 @@ namespace Bank_Web_Application.Controllers
             }
 
             return Unauthorized();
+        }
+
+        [HttpGet]
+        public IEnumerable<Account> GetAccounts([FromQuery] int id) 
+        {
+            client = new RestClient(DataService);
+            RestRequest request = new RestRequest("api/accounts/holder/{i}", Method.Get);
+            request.AddUrlSegment("i", id);
+            RestResponse response = client.Execute(request);
+            IEnumerable<Account> accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(response.Content);
+            return accounts;
+        }
+
+        [HttpPost]
+        public IActionResult Deposit([FromBody] Transaction transaction)
+        {
+            client = new RestClient(DataService);
+            RestRequest request = new RestRequest("api/transactions", Method.Post);
+
+            Transaction deposit = new Transaction();
+            deposit.AccountId = transaction.AccountId;
+            deposit.Amount = transaction.Amount;
+            deposit.DateTime = DateTime.Now;
+            deposit.Type = Transaction.TransactionType.Deposit;
+            request.AddBody(deposit);
+
+            RestResponse response = client.Execute(request);
+
+            IEnumerable<Account> accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(response.Content);
+            return new ObjectResult(deposit) { StatusCode = 201};
+        }
+
+        [HttpPost]
+        public IActionResult Withdraw([FromBody] Transaction transaction)
+        {
+            client = new RestClient(DataService);
+            RestRequest request = new RestRequest("api/transactions", Method.Post);
+
+            Transaction withdrawal = new Transaction();
+            withdrawal.AccountId = transaction.AccountId;
+            withdrawal.Amount = transaction.Amount;
+            withdrawal.DateTime = DateTime.Now;
+            withdrawal.Type = Transaction.TransactionType.Withdrawal;
+            request.AddBody(withdrawal);
+
+            RestResponse response = client.Execute(request);
+
+            IEnumerable<Account> accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(response.Content);
+            return new ObjectResult(withdrawal) { StatusCode = 201 };
         }
 
     }
